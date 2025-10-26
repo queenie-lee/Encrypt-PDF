a9eaac3330fd4aa70626f54ebea5f7702dc6ad50
Queenie Lee <122368579+queenie-lee@users.noreply.github.com>
2025-10-26 20:17:25 +0000

    First version of working code

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+import json
 import os
 import shutil
 
 from pypdf import PdfReader, PdfWriter
 
+
 def app():
-    source = "./files/input"
-    destination = "./files/output"
-    completed = "./files/completed"
-    input_file = "input.pdf"
+    with open("config.json", "r") as jf:
+        data = json.load(jf)  # reading json file
+        jf.close()
+
+    source = data["source"]
+    destination = data["destination"]
+    completed = data["completed"]
+    password = data["password"]
+
+    # Get list of files in the source directory
+    input_files = os.listdir(source)
+
+    for file in input_files:
+        read_encrypt_save_file(file, password, destination, source, completed)
+
+
+def read_encrypt_save_file(input_file, password, destination, source, completed):
 
     input_file_path = os.path.join(source, input_file)
-    output_file_path = os.path.join(destination, "output.pdf")
     reader = PdfReader(input_file_path)
     writer = PdfWriter(clone_from=reader)
 
     # Add a password to the new PDF
-    writer.encrypt("a_password", algorithm="AES-256-R5")
+    writer.encrypt(password, algorithm="AES-256-R5")
+
+    output_file_path = os.path.join(destination, input_file)
 
     # Save the new PDF to a file
     with open(output_file_path, "wb") as f:
         writer.write(f)
+        print(f"Encrypted {input_file} and saved to {output_file_path}")
 
     # Move content of source to completed
     shutil.move(input_file_path, os.path.join(completed, input_file))
+    print(f"Moved {input_file} to {completed}")
+
 
 if __name__ == "__main__":
     app()
